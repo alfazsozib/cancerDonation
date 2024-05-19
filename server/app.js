b1e2b1ba55e2852a7a05a3c067f6216b03b51882
alfazsozib <alfazsozib.com>
2024-05-20 02:03:54 +0600

    login complete

--- server/app.js
+++ server/app.js
@@ -4,41 +4,60 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const User = require('./models/User');
+const cors = require('cors');
+
+
+
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 8080;
 
 app.use(bodyParser.json());
+app.use(cors());
 
-// Connect to MongoDB
 mongoose.connect('mongodb+srv://aspro1141:EhUWzcK0uzIDs6i7@donate.nyuu3rd.mongodb.net/?retryWrites=true&w=majority&appName=donate')
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Error connecting to MongoDB:', err));
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Error connecting to MongoDB:', err));
 
 // User registration endpoint
 app.post('/register', async (req, res) => {
-  try {
-    const userData = req.body;
-    const newUser = await User.create(userData);
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+    try {
+        const {firstName, lastName, email, password} = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            res.status(400).json({ message: 'Email already exists' });
+        }else {
+            res.status(201).json({ message: 'User Created' });
+        }
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            password
+        });
+
+        await newUser.save();
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 });
+
 
 // User login endpoint
 app.post('/login', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email, password });
-    if (user) {
-      res.json({ message: 'Login successful', user });
-    } else {
-      res.status(401).json({ message: 'Invalid credentials' });
+    try {
+        const { email, password } = req.body;
+        console.log(email,password)
+        const user = await User.findOne({ email, password });
+        if (user) {
+            res.json({ message: 'Login successful', user });
+        } else {
+            res.status(401).json({ message: 'Invalid credentials' });
+        }
+    } catch (error) {
+        res.status(400).json({ message: error.message });
     }
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
 });
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
--- src/component/Login/LoginModal.jsx
+++ src/component/Login/LoginModal.jsx
@@ -1,46 +1,182 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { GiCrossMark } from "react-icons/gi";
+import axios from "axios";
 
 function LoginModal({ setShowModal }) {
+  const [register, setRegister] = useState(false);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
 
-    const [register, setRegister] = useState(false);
-    const closeModal = () => {
-        setShowModal(false);
-    };
+  const [loginData, setLoginData] = useState({
+    email:"",
+    password:""
+  })
 
-    const toggleRegister = () => {
-        setRegister(prevState => !prevState);
-    };
+  const closeModal = () => {
+    setShowModal(false);
+  };
 
-    return (
-        <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-75'>
-            <div className='w-[50rem] relative flex justify-center items-center h-96 bg-[#001c25b0] rounded-xl'>
-                <div className='absolute top-2 cursor-pointer right-4'>
-                    <GiCrossMark onClick={closeModal}  size={40} color='white'/>
-                </div>
-                <div>
-                    <div className='flex flex-col gap-4'>
-                        {register ? (
-                            <>
-                                <input className='px-20 py-2 rounded-lg' type="text" placeholder='Enter First Name' />
-                                <input className='px-20 py-2 rounded-lg' type="text" placeholder='Enter Last Name' />
-                                <input className='px-20 py-2 rounded-lg' type="email" placeholder='Enter Email' />
-                                <input className='px-20 py-2 rounded-lg' type="text" placeholder='Enter Password' />
-                                <button onClick={toggleRegister} className='bg-[#000000] font-serif text-white hover:bg-white hover:text-black font-bold py-2 rounded-lg'>Register</button>
-                            </>
-                        ) : (
-                            <>
-                                <input className='px-20 py-2 rounded-lg' type="text" placeholder='Enter Email Address' />
-                                <input className='px-20 py-2 rounded-lg' type="text" placeholder='Enter Password' />
-                                <button className='bg-[#000000] font-serif text-white hover:bg-white hover:text-black font-bold py-2 rounded-lg'>Login</button>
-                                <button onClick={toggleRegister} className='bg-[#000000] font-serif text-white hover:bg-white hover:text-black font-bold py-2 rounded-lg'>Register</button>
-                            </>
-                        )}
-                    </div>
-                </div>
-            </div>
+  const toggleRegister = () => {
+    setRegister((prevState) => !prevState);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleChangeLogin = (e) => {
+    const { name, value } = e.target;
+    setLoginData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    
+    try {
+        const response = await axios.post("http://localhost:8080/register", formData);
+        console.log(response)
+        if (response.status === 201) {
+            console.log("Registration successful");
+            alert("Registration successful")
+        }
+        
+    } catch (error) {
+        if (error.response && error.response.status === 400) {
+                alert("Email already exists")
+        } 
+        if (error.response && error.response.status === 201) {
+            alert("Registration successful")
+    } else {
+        console.error("Error during registration:", error.message);
+    }
+    }
+
+    
+};
+
+const handleSubmitLogin = async (e) => {
+    e.preventDefault();
+    console.log(loginData);
+    try {
+        const response = await axios.post("http://localhost:8080/login", loginData);
+
+        if (response.status === 200) {
+            console.log("Login successful");
+            alert("Login Successful");
+        }
+        console.log(response.status)
+    } catch (error) {
+        if (error.response.status == 401 && error.response.data.message === 'Invalid credentials') {
+            alert("User Not Exits With this Email Please Register First.....");
+        }
+        console.error("Error during login:", error.message);
+    }
+};
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75">
+      <div className="w-[50rem] relative flex justify-center items-center h-96 bg-[#001c25b0] rounded-xl">
+        <div className="absolute top-2 cursor-pointer right-4">
+          <GiCrossMark onClick={closeModal} size={40} color="white" />
         </div>
-    );
+        <div>
+          <div className="flex flex-col gap-4">
+            {register ? (
+              <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
+                <input
+                  className="px-20 py-2 rounded-lg"
+                  type="text"
+                  name="firstName"
+                  placeholder="Enter First Name"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                />
+                <input
+                  className="px-20 py-2 rounded-lg"
+                  type="text"
+                  name="lastName"
+                  placeholder="Enter Last Name"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                />
+                <input
+                  className="px-20 py-2 rounded-lg"
+                  type="email"
+                  name="email"
+                  placeholder="Enter Email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+                <input
+                  className="px-20 py-2 rounded-lg"
+                  type="text"
+                  name="password"
+                  placeholder="Enter Password"
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+
+                <button
+                  onClick={handleSubmit}
+                  className="bg-[#000000] font-serif text-white hover:bg-white hover:text-black font-bold py-2 rounded-lg"
+                >
+                  Register
+                </button>
+              </form>
+            ) : (
+              <>
+                (
+                <>
+                {/* "Login Section" */}
+                <form className="flex flex-col gap-4" onSubmit={handleSubmitLogin}>
+                  <input
+                    className="px-20 py-2 rounded-lg"
+                    type="text"
+                    name="email"
+                    value={loginData.email}
+                    onChange={handleChangeLogin}
+                    placeholder="Enter Email Address"
+                  />
+                  <input
+                    className="px-20 py-2 rounded-lg"
+                    type="text"
+                    name="password"
+                    value={loginData.password}
+                    onChange={handleChangeLogin}
+                    placeholder="Enter Password"
+                  />
+                  <button className="bg-[#000000] font-serif text-white hover:bg-white hover:text-black font-bold py-2 rounded-lg">
+                    Login
+                  </button>
+                  <button
+                  onClick={toggleRegister}
+                    className="bg-[#000000] font-serif text-white hover:bg-white hover:text-black font-bold py-2 rounded-lg"
+                  >
+                    Register
+                  </button>
+                  </form>
+                </>
+                )
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginModal;
